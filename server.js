d8e3cfbf4753df95c36c3e482fa66f76eb0a2a15
ellzmarie <elliematutisdigal@gmail.com>
2022-10-17 21:36:46 -0700

    add food options

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const port = 3005
 app.use(express.static('public'))
 
 const drinks = require('./models/drinks')
+const food = require('./models/food')
 
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
 
-// index
+// drinks index
 app.get("/drinks", (req, res) => {
     // res.send(drinks)
     
@@ -26,10 +27,33 @@ app.get("/drinks/:id", (req, res) => {
     })
 })
 
-// show
+// foods index
+
+app.get("/food", (req, res) => {    
+    res.render('food_index.ejs', {
+        allFood: food,
+        title: 'index'
+    })
+})
+
+app.get("/food/:id", (req, res) => {
+    res.render('food_show.ejs', {
+        food: food[req.params.id]
+    })
+})
+
+// drinks show
 app.get('/drinks/:id', (req, res) => {
     res.render('show.ejs', {
         drinks: drinks[req.params.id],
+        title: 'show'
+    })
+})
+
+// foods show
+app.get('/food/:id', (req, res) => {
+    res.render('food_show.ejs', {
+        food: food[req.params.id],
         title: 'show'
     })
 })
